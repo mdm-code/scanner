61693fd1b907de59dc9678a002af671ba8fdad88
mdm-code <code.madamczyk@gmail.com>
2023-09-24 23:01:52 +0200

    Fix the single typo in the codebase

--- example_test.go
+++ example_test.go
@@ -30,7 +30,7 @@ func ExampleScanner_Scan() {
 
 // ExampleScanner_Reset shows how to reset the scanner back to its initial,
 // zero state. In the example, tokens produced by the scanner the usual way are
-// discarded, and then the scanner gets reset back to its inital state.
+// discarded, and then the scanner gets reset back to its initial state.
 func ExampleScanner_Reset() {
 	r := strings.NewReader("Hello!")
 	s, err := scanner.New(r)
